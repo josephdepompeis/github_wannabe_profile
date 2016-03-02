5fcfdfb169827a50436b78988c5c6a8782128ee9
Joseph DePompeis <josephdepompeis@gmail.com>
2016-03-01 23:20:02 -0500

    update git display

--- app/controllers/form_controller.rb
+++ app/controllers/form_controller.rb
@@ -1,7 +1,11 @@
 class FormController < ApplicationController
   def ask
+
+
   end
 
   def show
+
+  @user = params[:git_name]
   end
 end
--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,6 @@
 class Form
   def initialize(git_name)
-    @response = HTTParty.get("https://api.github.com/users/#{git_name}/repos?access_token=#{ENV['GIT_KEY']}")
+    @repos = HTTParty.get("https://api.github.com/users/#{git_name}/repos?access_token=#{ENV['GIT_KEY']}")
+    @user = HTTParty.get("https://api.github.com/users/#{git_name}?access_token=#{ENV['GIT_KEY']}")
   end
 end
